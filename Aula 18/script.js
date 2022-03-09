45f1ed68e1dc0d5474da48d587260f7f4a7a71aa
profbreno <brenod123@gmail.com>
2022-03-08 21:55:44 -0300

    Aula 08/03

---
+++ Aula 18/script.js
@@ -0,0 +1,12 @@
+var x = 3; // declaração de variável
+var y = "hello"; // declaração de variável
+var z; // undefined
+alert("Hello World! x = " + x); // imprime na tela um popup com a mensagem Hello World! e a variável x
+
+console.log("Hello World! x = " + x);
+console.log(typeof x); // number
+console.log(typeof y); // string
+console.log(typeof z); // undefined
+
+alert(10 == "10"); // true
+alert(10 === "10"); // false
