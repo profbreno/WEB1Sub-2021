e88a41c7ca805d14514575ba08c2ba11475aa975
profbreno <brenod123@gmail.com>
2022-03-15 21:59:38 -0300

    Aula 15/03

--- Aula 19/script.js
+++ Aula 19/script.js
@@ -3,8 +3,26 @@ function setElementBackground(elemento) {
   var green = Math.floor(Math.random() * 256);
   var blue = Math.floor(Math.random() * 256);
 
-  var objeto = document.getElementById(elemento);
-  if (objeto) {
-    objeto.style.background = "rgb(" + red + "," + green + ", " + blue + ")";
+  if (elemento != undefined) {
+    var objeto = document.getElementById(elemento);
+    if (objeto) {
+      objeto.style.background = "rgb(" + red + "," + green + ", " + blue + ")";
+    }
   }
+  return [red, green, blue];
 }
+
+function findMax() {
+  var i,
+    max = 0;
+  for (i = 0; i < arguments.length; i++) {
+    if (arguments[i] > max) {
+      max = arguments[i];
+    }
+  }
+  return max;
+}
+let x = findMax(1, 212, 31, 404, 530, 60, 7, 89, 992, 210);
+let y = setElementBackground("botao");
+console.log(x);
+console.log(y);
